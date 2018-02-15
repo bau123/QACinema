8f2ed40251c099df169e9cedd3600f58b9d1288f
Bau Nguyen <bau.nguyen@kcl.ac.uk>
2018-02-15 11:32:29 +0000

    Test for Wednesday deal
    Check to see if correct value is given when the Wednesday deal is on or
    off.

--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -3,14 +3,15 @@ import org.junit.Test;
 
 public class TicketTest {
 
-    boolean wedDeal = false;
+    boolean wedDeal;
     private int stnAmount = 1;
     private int OAPAmount = 0;
     private int studentAmount =1;
     private int childAmount = 1;
 
     @Test
-    public void getTotalPrice(){
+    public void getTotalDealOff(){
+        wedDeal = false;
         int i = 0;
         if (wedDeal){
             i = -2;
@@ -20,6 +21,20 @@ public class TicketTest {
         int student = 6 * studentAmount;
         int child = 4 * childAmount;
         i = stn + OAP + student + child + i;
-        Assert.assertEquals("Wrong Value", 18, i);
+        Assert.assertEquals("Wrong total", 18, i);
+    }
+    @Test
+    public void getTotalDealOn(){
+        wedDeal = true;
+        int i = 0;
+        if (wedDeal){
+            i = -2;
+        }
+        int stn = 8 * stnAmount;
+        int OAP = 6 * OAPAmount;
+        int student = 6 * studentAmount;
+        int child = 4 * childAmount;
+        i = stn + OAP + student + child + i;
+        Assert.assertEquals("Wrong Value", 16, i);
     }
 }
